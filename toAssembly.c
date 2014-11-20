4f800ee63873e471bffed4513eebfd84ab9443e1
adarevalo <adriel.arevalo@yahoo.com>
2014-11-20 20:43:46 +0800

    Create toAssembly.c

---
+++ toAssembly.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int x = 0;
+int y = 1;
+char z = 'a';
+char w[2] = "aa";
+char b[12] = "is the value";
+
+main(){
+
+printf("a");
+printf("1");
+printf("Hello World!");
+printf("hey");
+printf("Number %d %s %d %c", x,b,x,z);
+}
